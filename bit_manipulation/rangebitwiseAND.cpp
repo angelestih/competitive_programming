e881091da8d6d8f3bbdf173b8fb5909a6632aa30
angelestih <ade2119@columbia.edu>
2024-07-27 15:35:56 -0700

    updated

---
+++ bit_manipulation/rangebitwiseAND.cpp
@@ -0,0 +1,71 @@
+#include <bit>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> prefixsumBit(vector<int>& nums)
+{
+    int n = nums.size();
+
+    vector<vector<int>> temp(n+1, vector<int>(32, 0));
+    for (int i = 1; i <= n; ++i)
+    {
+        /* storing bit info in temp */
+        int num = nums[i-1];
+
+        for (int j = 0; j < 32; ++j)
+        {
+            if (((1 << j) & num) != 0)
+            {
+                temp[i][j] = 1;
+            }
+            
+        }
+        
+    }
+
+    vector<vector<int>> psum(n+1, vector<int>(32, 0));
+    for (int j = 0; j < 32; ++j)
+    {
+        for (int i = 1; i <= n; ++i)
+        {
+            psum[i][j] = psum[i-1][j] + temp[i][j];
+        }
+        
+    }
+    return psum;
+    
+    
+}
+
+
+int rangeBitwiseAND(vector<vector<int>>& psum, int l, int r)
+{
+    int result = 0;
+    for (int j = 0; j < 32; ++j)
+    {
+        /* calculate the count of eleents with jth bit set in range[l, r] */
+        int count = psum[r][j] - psum[l-1][j];
+
+        if (count == r-l+1)
+        {
+            /* if all elements in range have jth bit set, add to result */
+            result = result + (1<<j);
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    vector<int> nums = {13, 11, 2, 3, 6};
+
+    int l =2, r = 4;
+
+    vector<vector<int>> psum = prefixsumBit(nums);
+
+    cout << "Bitwise AND of range [2,4] is :"
+         << rangeBitwiseAND(psum, l, r);
+    
+    return 0;
+}
